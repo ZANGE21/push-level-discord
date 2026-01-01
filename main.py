4f389bdd407cca33d52109c4f70652598818dccb
ZANGETSU <71890472+ZANGE21@users.noreply.github.com>
2026-01-02 02:27:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import time
-import os                                               
+import os 
+
 def get_last_message_id(channel_id, headers, target_user_id):                                                       r = requests.get(
         f"https://discord.com/api/v9/channels/{channel_id}/messages?limit=5",
         headers=headers
